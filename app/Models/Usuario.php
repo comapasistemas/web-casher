59bed05281da96cd7ef4137a4ff2cb63709fdb7b
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-07-22 14:24:09 -0600

    Factoriza query codificacion y decodificacion  de campos con MySQL

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Usuario extends Model
 {
@@ -33,5 +34,15 @@ class Usuario extends Model
         ];
 
         return implode(' ', $data);
+    }
+
+    private static function encodePasswordByMySQL(string $value)
+    {
+        return DB::select( DB::raw("SELECT ENCODE({$value}, 'hackcomapa') as encoded") );
+    }
+
+    private static function decodePasswordByMySQL(int $id)
+    {
+        return DB::select( DB::raw("SELECT DECODE(password, 'hackcomapa') as decoded from usuarios where id = {$id}") );
     }
 }
