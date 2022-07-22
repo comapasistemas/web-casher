09a5ed0568ea9e81adc705183926d02fec367f57
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-07-22 15:31:18 -0600

    Remueve linea de uso de clase

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UsuarioRequest;
 use App\Models\Usuario;
-use Illuminate\Support\Facades\Hash;
 
 class UsuarioController extends Controller
 {
