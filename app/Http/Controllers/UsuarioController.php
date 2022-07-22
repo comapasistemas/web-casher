8589b9a477b0d0a4b81991743bc10f5515dfaf0e
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-07-22 15:16:38 -0600

    Factoriza store de Usuario

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -5,21 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UsuarioRequest;
 use App\Models\Usuario;
+use Illuminate\Support\Facades\Hash;
 
 class UsuarioController extends Controller
 {
     public function index()
 	{
-		return view('usuarios.index')->with('usuarios', Usuario::orderBy('id', 'desc')->take(100)->get());
+		return view('usuarios.index', [
+			'usuarios' => Usuario::orderBy('id', 'desc')->take(100)->get(),
+		]);
 	}
 
 	public function create()
 	{
-		return view('usuarios.create')->with('usuario', new Usuario);
+		return view('usuarios.create');
 	}
 
 	public function store(UsuarioRequest $request)
 	{
-		return $request->validated();
+		if(! $usuario = Usuario::create($request->validated()))
+			return back()->with('message', 'Intenta registrarte nuevamente');
+
+		return redirect()->route('autenticacion.entrar')->with('message', 'Usuario registrado');
 	}
 }
