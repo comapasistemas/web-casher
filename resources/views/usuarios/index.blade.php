de91bf455b9190331e7b7d5a8e70f7df06187947
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-07-22 14:24:56 -0600

    Actualiza view index de usuarios

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -1,16 +1,18 @@
 @extends('app')
 @section('content')
-	<table>
-		<thead>
-		</thead>
-		<tbody>
-			@foreach($usuarios as $usuario)
-			<tr>
-				<td>{{ $usuario->id }}</td>
-				<td>{{ $usuario->nombre_completo }}</td>
-				<td>{{ $usuario->email }}</td>
-			</tr>
-			@endforeach
-		</tbody>
-	</table>
+<table>
+	<thead>
+	</thead>
+	<tbody>
+		@foreach($usuarios as $usuario)
+		<tr>
+			<td>{{ $usuario->id }}</td>
+			<td>{{ $usuario->nombre_completo }}</td>
+			<td>{{ $usuario->usuario }}</td>
+			<td>{{ $usuario->password }}</td>
+			<td>{{ $usuario->email }}</td>
+		</tr>
+		@endforeach
+	</tbody>
+</table>
 @endsection
