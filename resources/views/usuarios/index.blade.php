f70e93d4d5cb6f81423b725a1d07c66fe8c4dba5
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-07-27 12:07:55 -0600

    Actualiza temporalmente index de Usuario

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -4,22 +4,24 @@
 	<thead>
 		<tr>
 			<th>ID</th>
-			<th>NOMBRE COMPLETO</th>
-			<th>CORREO ELECTRONICO</th>
+			<!-- <th>NOMBRE COMPLETO</th> -->
+			<!-- <th>CORREO ELECTRONICO</th> -->
 			<th>USUARIO</th>
 			<th>PASSWORD</th>
 			<th>DECODED</th>
+			<th>SECRET</th>
 		</tr>
 	</thead>
 	<tbody>
 		@foreach($usuarios as $usuario)
 		<tr>
 			<td>{{ $usuario->id }}</td>
-			<td>{{ $usuario->nombre_completo }}</td>
-			<td>{{ $usuario->email }}</td>
+			<!-- <td>{{ $usuario->nombre_completo }}</td> -->
+			<!-- <td>{{ $usuario->email }}</td> -->
 			<td>{{ $usuario->usuario }}</td>
 			<td>{{ $usuario->password }}</td>
 			<td>{{ $usuario->decoded }}</td>
+			<td>{{ $usuario->secretword }}</td>
 			<td>
 				<a href="{{ route('usuarios.edit', $usuario) }}">Editar</a>
 			</td>
