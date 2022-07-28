bf993157ce0af47bef56fe360d23f3627261b36e
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-07-28 09:22:57 -0600

    Reestructura y factoriza view de contrato de servicio de la aplicacion

---
+++ app/Http/Controllers/ContratoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContratoController extends Controller
+{
+    public function clausulas()
+    {
+        return view('contrato.index');
+    }
+}
