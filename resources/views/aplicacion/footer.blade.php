d9af181bba77f04c652caf172c51f6e10ef59625
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-07-29 12:06:50 -0600

    Actualiza contenido del footer de la aplicacion

--- resources/views/aplicacion/footer.blade.php
+++ resources/views/aplicacion/footer.blade.php
@@ -1,7 +1,7 @@
 <hr>
 <div>
     <small>
-        <p>Comapa de Nuevo Laredo, Tamps. México {{ date('Y') }}  @ Todos los derechos reservados</p>
+        <p>COMAPA DE NUEVO LAREDO, MÉXICO | TODOS LOS DERECHOS RESERVADOS @ {{ date('Y') }}</p>
         <p>
             <a href="https://www.facebook.com/comapa.nuevolaredo/" target="_blank">Facebook</a>
             <a href="https://twitter.com/ComapaNvoLaredo" target="_blank">Twitter</a>
