b34b3040f928f2bf1bc718e07b266e7cc41fc657
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-08-01 11:21:28 -0600

    Factoriza config de tiempo para manejo de nombres de meses y dias en diferentes idiomas

---
+++ config/comuapoa/tiempo.php
@@ -0,0 +1,59 @@
+<?php 
+
+return [
+    /**
+     * Lenguaje español
+     * 
+     */
+    'meses' => [
+        1 => 'enero',
+        2 => 'febrero',
+        3 => 'marzo',
+        4 => 'abril',
+        5 => 'mayo',
+        6 => 'junio',
+        7 => 'julio',
+        8 => 'agosto',
+        9 => 'septiembre',
+        10 => 'octubre',
+        11 => 'noviembre',
+        12 => 'diciembre',
+    ],
+    'dias' => [
+        1 => 'lunes',
+        2 => 'martes',
+        3 => 'miércoles',
+        4 => 'jueves',
+        5 => 'viernes',
+        6 => 'sábado',
+        7 => 'domingo',
+    ],
+
+    /**
+     * English language
+     * 
+     */
+    'months' => [
+        1 => 'january',
+        2 => 'february',
+        3 => 'march',
+        4 => 'april',
+        5 => 'may',
+        6 => 'june',
+        7 => 'july',
+        8 => 'august',
+        9 => 'september',
+        10 => 'october',
+        11 => 'november',
+        12 => 'december',
+    ],
+    'days' => [
+        1 => 'monday',
+        2 => 'tuesday',
+        3 => 'wednesday',
+        4 => 'thursday',
+        5 => 'friday',
+        6 => 'saturday',
+        7 => 'sunday',
+    ],
+];
