7a0cef7a5a24fb140261c8a54709850903eba679
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-08-01 10:27:55 -0600

    Factoria config bancario para agrupar el contexto del sistema bancario

---
+++ config/comuapoa/bancario.php
@@ -0,0 +1,27 @@
+<?php 
+
+return [
+    /**
+     * Nombres de bancos activos en la actualidad de México
+     * 
+     */
+    'bancos' => [
+        'Banamex',
+        'Banjercito',
+        'Banorte', 
+        'Banregio',
+        'BBVA', 
+        'HSBC',
+        'Santander',
+    ],
+
+    /**
+     * Redes globales para tarjetas de credito y débito
+     * 
+     */
+    'redes' => [
+        'AMERICAN EXPRESS',
+        'MASTERCARD',
+        'VISA',
+    ],
+];
