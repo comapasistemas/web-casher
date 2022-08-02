8ad99266a43536888a04bf377629155a02a48fd0
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-08-02 10:41:22 -0600

    Factoriza model FacturaPagar para migracion de fact_ant

---
+++ app/Models/FacturaPagar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FacturaPagar extends Model
+{
+    use HasFactory;
+
+    protected $table = 'fact_ant';
+}
