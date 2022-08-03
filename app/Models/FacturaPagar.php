28e527076dd1575847a802c1f5e4779b8e24ee12
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-08-03 12:21:09 -0600

    Factoriza funcionalidades del periodo en el modelo FacturaPagar

--- app/Models/FacturaPagar.php
+++ app/Models/FacturaPagar.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+* Periodo se conforma de 6 digitos, los primeros 4(*) son el año y los ultimos 2(#) son el numero de mes
+* 
+Ejemplo: ****##
+*/
 class FacturaPagar extends Model
 {
     use HasFactory;
@@ -14,6 +19,29 @@ class FacturaPagar extends Model
     protected $guarded = [];
     
     public $timestamps = false;
+
+    /**
+     * PERIODO se forma con un número de 6 dígitos: 
+     * 
+     * Los primeros 4 dígitos son el año
+     * Los últimos 2 dígitos son el número del mes
+     * 
+     * Ejemplo: 202201 (2022 - año) (01 - número de mes: enero) 
+     */
+    public function getNumeroPeriodoAttribute()
+    {
+        return $this->PERIODO;
+    }
+
+    public function getMesPeriodoAttribute()
+    {
+        return substr($this->PERIODO, 4, 2);
+    }
+
+    public function getAnioPeriodoAttribute()
+    {
+        return substr($this->PERIODO, 0, 4);
+    }
 
     public function ruta()
     {
