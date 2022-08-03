8f2cdfad27ce9dc36a472bb3d743d04b28f2bd3f
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-08-03 15:19:19 -0600

    Factoriza funcionalidad de fecha de vencimiento en modelo FacturaPagar

--- app/Models/FacturaPagar.php
+++ app/Models/FacturaPagar.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Ahex\Config\Application\Vencimiento;
+use App\Ahex\Config\Application\Tiempo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -51,5 +53,19 @@ class FacturaPagar extends Model
     public function pagoConvenios()
     {
         return $this->hasMany(PagoConvenio::class, 'convenio', 'REZAGO_MAN');
+    }
+
+    public function fechaVencimiento(int $clave_vencimiento, bool $mostrar_nombre_mes = true, string $separator = '/')
+    {
+        $fecha_vencimiento_data = [
+            'dia' => Vencimiento::getDiaPorClave($clave_vencimiento),
+            'mes' => $mostrar_nombre_mes ? Tiempo::nombreMes($this->mes_periodo) : $this->mes_periodo,
+            'año' => $this->anio_periodo,
+        ];
+
+        if( Vencimiento::isUltimaClave($clave_vencimiento) && ($this->mes_periodo == 02) )
+            $fecha_vencimiento_data['dia'] = '28';
+
+        return implode($separator, $fecha_vencimiento_data);
     }
 }
