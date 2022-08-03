53df2fb2621bf0118917e93e78a2ee5b0aa67880
Comapa Sistemas <comapa.sistemas@gmail.com>
2022-08-03 11:30:11 -0600

    Factoriza config de claves de vencimiento

---
+++ config/comuapoa/claves_vencimiento.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Arreglo de claves de vencimiento
+ * 
+ * [numero clave => dia del mes]
+ * @return array
+ */
+return [
+    1 => '05',
+    2 => '10',
+    3 => '15',
+    4 => '20',
+    5 => '25',
+    6 => '30',
+];
